bf4a8cae4d9d2fbb5cce06551878dbf0d1c13b51
skyleaworlder <870033938@qq.com>
2020-12-07 20:01:07 +0800

    A: add back_end, init back-end with flask

---
+++ back_end/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, jsonify
+import os
+import subprocess
+from flask import Response, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def test():
+
+    # execute .exe file
+    cmd = os.getcwd()+"/src/test.exe"
+    p = subprocess.Popen(
+        cmd, shell=True,
+        stdout=subprocess.PIPE
+    )
+
+    # process output
+    byte_stream = p.stdout.read()
+    str_stream = byte_stream.decode('gbk')
+    line_lst = str_stream.split('\r\n')
+    line_lst = [line.strip(" ") for line in line_lst if len(line) > 0]
+
+    return jsonify({ "res": line_lst })
+
+@app.route("/", methods=["POST"])
+def process():
+
+    data = request.get_json()
+    code = data["code"]
+
+    f = open("code.txt", "w")
+    f.write(code)
+
+    # execute .exe file
+    cmd = os.getcwd()+"/src/test.exe"
+    p = subprocess.Popen(
+        cmd, shell=True,
+        stdout=subprocess.PIPE
+    )
+
+    # process output
+    byte_stream = p.stdout.read()
+    str_stream = byte_stream.decode('gbk')
+    line_lst = str_stream.split('\r\n')
+    line_lst = [line.strip(" ") for line in line_lst if len(line) > 0]
+
+    return jsonify({ "res": line_lst })
+
+
+if __name__ == "__main__":
+    app.run()
