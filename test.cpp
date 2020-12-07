1e1ae8a412bb60f954fe39e2b852ed6a3d6dd413
skyleaworlder <870033938@qq.com>
2020-12-08 00:47:32 +0800

    M: add tree test in test.cpp

--- test.cpp
+++ test.cpp
@@ -1,10 +1,16 @@
 #include "utils.hpp"
-#include "baseGramma.hpp"
+#include "tree.hpp"
+
 #include "LR1.hpp"
+#include "baseGramma.hpp"
+
+
+#include "lexer.hpp"
 #include <string>
 #include <vector>
 #include <set>
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
@@ -172,6 +178,7 @@ void test_calcuClosePkg() {
     for (const auto& lr0 : g.lr0_derivations) {
         cout << lr0.id << ": (" << lr0.left.id << ", " << lr0.left.name << ")" << endl;
         cout << "dot pos: " << lr0.dot_position << endl;
+        cout << "derive idx: " << lr0.derive_idx << endl;
         for (auto elem : lr0.right)
             cout << "(" << elem.id << ", " << elem.name << ") ";
         cout << endl << endl;
@@ -240,6 +247,110 @@ void test_actiongoto() {
 	}
 }
 
+void test_tree() {
+    using std::vector;
+
+    list<token> arr = {
+        {2,1,"a","a"},
+        {2,1,"a","a"},
+        {3,1,"b","b"},
+        {2,1,"a","a"},
+        {3,1,"b","b"},
+        {0,1,"#","#"}
+    };
+    /*
+    list<token> arr = {
+        { 0, 1, "int", "int" },
+        { 29, 1, "<ID>", "main" },
+        { 23, 1, "(", "(" },
+        { 24, 1, ")", ")"},
+        { 25, 1, "{", "{" },
+        { 0, 2, "int", "int" },
+        { 29, 2, "<ID>", "a" },
+        { 12, 2, "=", "=" },
+        { 30, 2, "<INT>", "1" },
+        { 27, 2, ";", ";" },
+        { 26, 3, "}", "}" }
+    };
+    */
+
+    LR1Gramma g;
+    g.readGramma("testgramma.txt");
+    g.calcuAllTerminalFirstSet();
+    g.calcuAllUnterminalFirstSet();
+    g.calcuLR0Derivations();
+    g.calcuLR1Derivations();
+    g.calcuActionTable();
+    g.calcuGotoTable();
+
+    cout << "ClosePkg: " << endl;
+    for (const auto& I : g.C) {
+        cout << I.id << ":" << endl;
+        for (auto drv : I.LR1_derivation_arr) {
+            cout << "(derivation, " << drv.lr0_derivation_idx << ") "
+                << "(lf_sym, " << drv.look_forward_symbol_idx << ")" << endl;
+        }
+        cout << endl << endl;
+    }
+
+    cout << g.action_table.size() << endl;;
+    for (const auto& elem : g.action_table) {
+        const close_pkg_idx c_idx = elem.first.first;
+        const symbol_idx s_idx = elem.first.second;
+        const ActionDetail actiondetail = elem.second;
+        cout << "(" << c_idx << ", " << g.symbol_arr[s_idx].name << ") => ("
+            << actiondetail.action << ", " << actiondetail.toward << ")" << endl;
+    }
+    cout << endl << endl;
+
+    for (const auto& elem : g.goto_table) {
+        const close_pkg_idx c_idx = elem.first.first;
+        const symbol_idx s_idx = elem.first.second;
+        const ActionDetail actiondetail = elem.second;
+        cout << "(" << c_idx << ", " << g.symbol_arr[s_idx].name << ") => ("
+            << actiondetail.action << ", " << actiondetail.toward << ")" << endl;
+    }
+
+    std::cout << "begin..." << std::endl;
+    tree a(arr, g.derivation_set, g.action_table, g.goto_table);
+
+    std::cout << "root child size: " << a.tree_root.child.size() << std::endl;
+    std::cout << "if record is 2, then success: " << a.tree_root.record << std::endl;
+    TreeNode root = *(a.tree_root.child[0]);
+
+    std::cout << "(0, " << root.id << ", " << root.name << ", "
+        << root.child.size() << ")" << std::endl;
+    std::queue<TreeNode*> q;
+    std::queue<TreeNode*> tmp;
+    for (auto elem : root.child)
+        q.push(elem);
+
+    size_t layer = 1;
+    do {
+        TreeNode* front = q.front();
+
+        q.pop();
+        //std::cout << "q size: " << q.size() << std::endl;
+        std::cout << "(" << layer << ", " << front->id << ", " << front->name << ", "
+            << front->child.size() << ")" << std::endl;
+        for (auto cd : front->child) {
+            tmp.push(cd);
+        }
+
+        //std::cout << "q size: " << q.size() << std::endl;
+        if (q.size() == 0) {
+            layer++;
+            while (tmp.size() != 0) {
+                //std::cout << "tmp size: " << tmp.size() << std::endl;
+                q.push(tmp.front());
+                tmp.pop();
+                //std::cout << q.size() << std::endl;
+            }
+        }
+
+    } while (!(q.size() == 0 && tmp.size() == 0));
+}
+
 int main() {
     //test_split();
     //test_set();
@@ -251,5 +362,6 @@ int main() {
     //test_cpp_static_method();
     //test_calcuClosePkg();
     //test_forrange();
-    test_actiongoto();
+    //test_actiongoto();
+    test_tree();
 }
