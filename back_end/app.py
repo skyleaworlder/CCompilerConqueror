a5d4024f99f4e52f36ba6a4a2cb3e2c894c804e6
skyleaworlder <870033938@qq.com>
2020-12-09 13:37:10 +0800

    M: add try-except, add password????

--- back_end/app.py
+++ back_end/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 import os
 import subprocess
 from flask import Response, request
+import traceback
 
 app = Flask(__name__)
 
@@ -29,28 +30,37 @@ def test():
 @app.route("/", methods=["POST"])
 def process():
 
-    data = request.get_json()
-    code = data["code"]
+    try:
+        data = request.get_json()
+        code = data["code"]
+        password = data["password"]
 
-    f = open("code.txt", "w")
-    f.write(code)
+        if password != "skyleaworlder":
+            return jsonify({ "tree_str": "error" })
 
-    # execute .exe file
-    # TODO: wait path
-    #cmd = os.getcwd()+"/src/test.exe"
-    cmd = "../test2.exe"
-    p = subprocess.Popen(
-        cmd, shell=True,
-        stdout=subprocess.PIPE
-    )
+        f = open("code.txt", "w")
+        f.write(code)
 
-    # process output
-    byte_stream = p.stdout.read()
-    str_stream = byte_stream.decode('gbk')
-    line_lst = str_stream.split('\r\n')
-    line_lst = [line.strip(" ") for line in line_lst if len(line) > 0]
+        # execute .exe file
+        # TODO: wait path
+        #cmd = os.getcwd()+"/src/test.exe"
+        cmd = "../test2.exe"
+        p = subprocess.Popen(
+            cmd, shell=True,
+            stdout=subprocess.PIPE
+        )
 
-    return jsonify({ "tree_str": line_lst })
+        # process output
+        byte_stream = p.stdout.read()
+        str_stream = byte_stream.decode('gbk')
+        line_lst = str_stream.split('\r\n')
+        line_lst = [line.strip(" ") for line in line_lst if len(line) > 0]
+
+        return jsonify({ "tree_str": line_lst })
+
+    except Exception as e:
+        traceback.print_exc()
+        return jsonify({ "tree_str": "failed" })
 
 
 if __name__ == "__main__":
