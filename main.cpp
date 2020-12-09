ac5dfce1ce07934d0f86ce2a0fbdc1c748bd21b5
skyleaworlder <870033938@qq.com>
2020-12-09 20:28:16 +0800

    M: fix Terminals

--- main.cpp
+++ main.cpp
@@ -13,16 +13,25 @@
 
 int main() {
     const std::vector<std::string> Terminals = {
-        "int", "float", "double", "void", "if", "else",
+        "#", "$", "int", "float", "double", "void", "if", "else",
         "while", "return", "+", "-", "*", "/", "=", "<", ">",
         "<=", ">=", "==", "!=", "+=", "-=", "*=", "/=",
-        "(", ")", "{", "}", ";", ","
+        "(", ")", "{", "}", ";", ",", "<ID>", "<INT>"
     };
 
     std::string to_lex = read_file("code.txt");
+    //std::cout << to_lex << std::endl;
+    //std::cout << "int main() {    int a = 1;    if ( a == 1 ) {        a += 1;    }    return 0;}" << std::endl;
     Lexer lex(Terminals);
     std::vector<Token> lex_res = lex.parse(to_lex);
     std::list<Token> token_stream(lex_res.begin(), lex_res.end());
+
+    /* debug
+    for (const auto& elem : token_stream) {
+        std::cout << elem.id << "," << elem.line_number << ","
+            << elem.name << ", " << elem.value << std::endl;
+    }
+    */
 
     LR1Gramma g;
     g.readGramma("gramma.txt");
