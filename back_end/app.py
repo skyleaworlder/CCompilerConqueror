eefc383a5208c5e9e0d4e0d8bed5dcc7957e3939
skyleaworlder <870033938@qq.com>
2020-12-09 11:41:51 +0800

    M: change return & get output changed from STDOUT to file

--- back_end/app.py
+++ back_end/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 import os
 import subprocess
 from flask import Response, request
+from functools import reduce
 
 app = Flask(__name__)
 
@@ -10,6 +11,7 @@ app = Flask(__name__)
 def test():
 
     # execute .exe file
+    parent_dir = os.path.dirname(os.getcwd())
     cmd = os.getcwd()+"/src/test.exe"
     cmd = "test2.exe"
     p = subprocess.Popen(
@@ -18,12 +20,15 @@ def test():
     )
 
     # process output
-    byte_stream = p.stdout.read()
-    str_stream = byte_stream.decode('gbk')
-    line_lst = str_stream.split('\r\n')
-    line_lst = [line.strip(" ") for line in line_lst if len(line) > 0]
+    f = open(parent_dir+"/data/tree.txt", "r")
+    tree_lst = f.readlines()
 
-    return jsonify({ "tree_str": line_lst })
+    # tree_str: 'Program\n0->1:GlbDefList\n1->2:GlbDef 3:GlbDefList\n
+    # tree_lst: ['Program', '0->1:GlbDefList', '1->2:GlbDef 3:GlbDefList']
+    tree_str = reduce(lambda sum, line: sum+line, tree_lst)
+    tree_lst = [line.strip('\n') for line in tree_lst]
+
+    return jsonify({ "tree_str": tree_str })
 
 
 @app.route("/", methods=["POST"])
@@ -32,8 +37,10 @@ def process():
     data = request.get_json()
     code = data["code"]
 
-    f = open("code.txt", "w")
+    parent_dir = os.path.dirname(os.getcwd())
+    f = open(parent_dir+"/data/code.txt", "w")
     f.write(code)
+    f.close()
 
     # execute .exe file
     # TODO: wait path
@@ -45,12 +52,15 @@ def process():
     )
 
     # process output
-    byte_stream = p.stdout.read()
-    str_stream = byte_stream.decode('gbk')
-    line_lst = str_stream.split('\r\n')
-    line_lst = [line.strip(" ") for line in line_lst if len(line) > 0]
+    f = open(parent_dir+"/data/tree.txt", "r")
+    tree_lst = f.readlines()
 
-    return jsonify({ "tree_str": line_lst })
+    # tree_str: 'Program\n0->1:GlbDefList\n1->2:GlbDef 3:GlbDefList\n
+    # tree_lst: ['Program', '0->1:GlbDefList', '1->2:GlbDef 3:GlbDefList']
+    tree_str = reduce(lambda sum, line: sum+line, tree_lst)
+    tree_lst = [line.strip('\n') for line in tree_lst]
+
+    return jsonify({ "tree_str": tree_str })
 
 
 if __name__ == "__main__":
